6f3a718d86b9f7599fad5d4e43846e230b0bd851
Chibi <chibi.m@gmail.com>
2022-10-03 18:07:59 +0200

    Test skipping & brushing up

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,10 +1,28 @@
+import pytest
 from pytest import mark
+import unittest
 
 
+#@mark.skip
 def test_env_is_qa(app_config):
-    assert app_config.base_url == "https://myqaenv.com"
-    assert app_config.app_port == 80
+    if app_config == "qa":
+        assert app_config.base_url == "https://myqaenv.com"
+        assert app_config.app_port == 80
+    elif app_config == "dev":
+        assert app_config.base_url == "https://mydevenv.com"
+        assert app_config.app_port == 8080
+    elif app_config == "staging":
+        assert app_config.base_url == "https://mystagingenv.com"
+        assert app_config.app_port == 666
+    else:
+        # TODO: this shit doesn't work to check exceptions if I enter a wrong env, gonna need to figure that out
+        try:
+            app_config
+        except Exception:
+            pass
 
-def test_env_is_dev(app_config):
-    assert app_config.base_url == "https://mydevenv.com"
-    assert app_config.app_port == 8080
+
+@mark.skip(reason="Just skipping this")
+def test_skipped():
+    pass
+
